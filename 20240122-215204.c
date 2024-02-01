97f404fa1e2a8d31f91bffe0599c5b25d476c854
VanshikaT0706 <158130680+VanshikaT0706@users.noreply.github.com>
2024-02-01 10:27:27 +0530

    Practice

---
+++ 20240122-215204.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+int N1=0,N2=0,sum=0;
+    printf("enter 1st no.=");
+    scanf("%d",&N1);
+    printf("enter 2nd no.=");
+    scanf("%d",&N2);
+    sum=N1+N2;
+    printf("\n addition of %d,&%d is =%d.\n",N1,N2,sum);
+    getch();
+    return 0;
+}
